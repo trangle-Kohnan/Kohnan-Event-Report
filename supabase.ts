f6f751a25d39e1a31f6772daf4d55337684d8797
trangle-Kohnan <trang.le@kohnanvn.com>
2026-02-02 15:42:11 +0700

    Update supabase.ts

--- supabase.ts
+++ supabase.ts
@@ -1,7 +1,8 @@
 
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://nvnylvxmmmuzehiodubj.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im52bnlsdnhtbW11emVoaW9kdWJqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkxODQxNzQsImV4cCI6MjA4NDc2MDE3NH0.lro9oPQDy-aKyZM0se8UIfW8zxvRxAsI4iB1BSGB374';
+// Gọi từ Vercel sang
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
